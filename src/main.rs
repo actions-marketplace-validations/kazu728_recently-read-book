67dc4962ed09e72030315e02d68be02e7d17c560
Kazuki Matsuo <kazuki.matsuo.728@gmail.com>
2023-01-23 12:51:56 +0900

    refactor: use process.exit(0)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::process;
+
 use recently_read_book::ApplicationError;
 
 extern crate recently_read_book;
@@ -5,7 +7,7 @@ extern crate recently_read_book;
 fn main() {
     recently_read_book::Config::init();
     match recently_read_book::run() {
-        Ok(_) => println!("Done"),
+        Ok(_) => process::exit(0),
         Err(ApplicationError::FetchError(e)) => panic!("Failed to fetch: {:?}", e),
         Err(ApplicationError::PublishError(e)) => panic!("Failed to publish: {:?}", e),
     }
